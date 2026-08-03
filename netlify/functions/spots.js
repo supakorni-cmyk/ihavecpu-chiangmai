a6101b9762c1c838888a4e086fa66c4731b1bd32
Boom <boomelm36@gmail.com>
2026-08-03 12:09:25 +0700

    IST > TST

--- netlify/functions/spots.js
+++ netlify/functions/spots.js
@@ -71,11 +71,8 @@ export default async (req) => {
         let adSpots = await spotsStore.get("spots-data", { type: "json" });
 
         // --- 2. THE FIX: Initialize if empty! ---
-        if (!adSpots) {
-            console.log("Database is empty. Initializing with default data...");
-            adSpots = INITIAL_DATA;
-            await spotsStore.setJSON("spots-data", adSpots);
-        }
+        adSpots = INITIAL_DATA;
+        await spotsStore.setJSON("spots-data", adSpots);
 
         return new Response(JSON.stringify(adSpots), { 
             status: 200,
